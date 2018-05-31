156592e20c4b1ab37230b39f830f6ebb6e36fd41
splintereddy <36838781+splintereddy@users.noreply.github.com>
2018-05-31 16:14:17 +0800

    Update ip_scan.sh

--- ip_scan.sh
+++ ip_scan.sh
@@ -5,10 +5,11 @@
 
 for i in $(cat iplist.txt)
 do
-	ping -c 1 $i
+	ping -c 1 $i > /dev/null 2>&1
 # $?表示生一次命令执行的结果，当命令运行成功时返回0.当执行失败时返回1
 # 当$?值为1时说明不能ping通，ip未被使用
 	if [[ $? == 1 ]]; then
-		echo $i >> ip_can_use.txt
+		echo $i > ip_can_use.txt
 	fi
 done
+echo "Finished! The result is saved in ip_can_use.txt!"
